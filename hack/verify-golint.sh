43e872971bafd70c542fde4f46f3a6fa2deb934f
Ernest Wong <chuwon@microsoft.com>
2019-11-07 12:46:22 -0800

    Bump csi-provisioner and csi-attacher version

--- hack/verify-golint.sh
+++ hack/verify-golint.sh
@@ -16,9 +16,8 @@
 
 set -euo pipefail
 
-curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | bash -s v1.17.1
+GO111MODULE=off go get -u github.com/golangci/golangci-lint/cmd/golangci-lint
 
-./bin/golangci-lint run --deadline=10m
+"$GOBIN"/golangci-lint run --deadline=10m
 
 echo "Congratulations! All Go source files have been linted."
-
