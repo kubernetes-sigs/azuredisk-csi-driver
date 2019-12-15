d64561f06e46dacd75fec98e72acb3d3920da049
Pengfei Ni <feiskyer@gmail.com>
2019-12-15 20:22:03 +0800

    Enable spell checking and fix spelling issues

--- hack/verify-all.sh
+++ hack/verify-all.sh
@@ -24,3 +24,4 @@ ${PKG_ROOT}/hack/verify-golint.sh
 ${PKG_ROOT}/hack/verify-dep.sh
 ${PKG_ROOT}/hack/verify-boilerplate.sh
 ${PKG_ROOT}/hack/verify-helm-chart.sh
+${PKG_ROOT}/hack/verify-spelling.sh
