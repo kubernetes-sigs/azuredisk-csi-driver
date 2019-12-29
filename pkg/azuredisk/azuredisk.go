3a111bb6924539ded328e3e142ef6e1a0029bdf5
andyzhangx <xiazhang@microsoft.com>
2019-12-29 03:06:33 +0000

    feat: BYOK(SSE+CMK) support

--- pkg/azuredisk/azuredisk.go
+++ pkg/azuredisk/azuredisk.go
@@ -21,25 +21,21 @@ import (
 	"fmt"
 	"reflect"
 	"regexp"
-	"strconv"
 	"strings"
-	"time"
 	"unicode"
 
 	csicommon "sigs.k8s.io/azuredisk-csi-driver/pkg/csi-common"
+	"sigs.k8s.io/azuredisk-csi-driver/pkg/mounter"
 
 	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2019-07-01/compute"
-	"github.com/Azure/go-autorest/autorest/to"
 	"github.com/container-storage-interface/spec/lib/go/csi"
 	"github.com/pborman/uuid"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	kwait "k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/klog"
 	"k8s.io/kubernetes/pkg/util/mount"
 	"k8s.io/kubernetes/pkg/volume/util"
 	"k8s.io/legacy-cloud-providers/azure"
-	"sigs.k8s.io/azuredisk-csi-driver/pkg/mounter"
 )
 
 const (
@@ -47,9 +43,6 @@ const (
 
 	errDiskNotFound = "not found"
 	// default IOPS Caps & Throughput Cap (MBps) per https://docs.microsoft.com/en-us/azure/virtual-machines/linux/disks-ultra-ssd
-	defaultDiskIOPSReadWrite = 500
-	defaultDiskMBpsReadWrite = 100
-
 	// see https://docs.microsoft.com/en-us/rest/api/compute/disks/createorupdate#uri-parameters
 	diskNameMinLength = 1
 	diskNameMaxLength = 80
@@ -77,13 +70,6 @@ var (
 	diskSnapshotPathRE      = regexp.MustCompile(`(?i).*/subscriptions/(?:.*)/resourceGroups/(?:.*)/providers/Microsoft.Compute/snapshots/(.+)`)
 	diskURISupportedManaged = []string{"/subscriptions/{sub-id}/resourcegroups/{group-name}/providers/microsoft.compute/disks/{disk-id}"}
 	diskURISupportedBlob    = []string{"https://{account-name}.blob.core.windows.net/{container-name}/{disk-name}.vhd"}
-
-	defaultBackOff = kwait.Backoff{
-		Steps:    20,
-		Duration: 2 * time.Second,
-		Factor:   1.5,
-		Jitter:   0.0,
-	}
 )
 
 // Driver implements all interfaces of CSI drivers
@@ -217,145 +203,6 @@ func isValidDiskURI(diskURI string) error {
 	}
 
 	return nil
-}
-
-// ManagedDiskOptions specifies the options of managed disks.
-type ManagedDiskOptions struct {
-	// The name of the disk.
-	DiskName string
-	// The size in GB.
-	SizeGB int
-	// The name of PVC.
-	PVCName string
-	// The name of resource group.
-	ResourceGroup string
-	// The AvailabilityZone to create the disk.
-	AvailabilityZone string
-	// The tags of the disk.
-	Tags map[string]string
-	// The SKU of storage account.
-	StorageAccountType compute.DiskStorageAccountTypes
-	// IOPS Caps for UltraSSD disk
-	DiskIOPSReadWrite string
-	// Throughput Cap (MBps) for UltraSSD disk
-	DiskMBpsReadWrite string
-	// if SourceResourceID is not empty, then it's a disk copy operation(for snapshot)
-	SourceResourceID string
-	// The type of source
-	SourceType string
-}
-
-//CreateManagedDisk : create managed disk
-func (d *Driver) CreateManagedDisk(ctx context.Context, options *ManagedDiskOptions) (string, error) {
-	var err error
-	klog.V(4).Infof("azureDisk - creating new managed Name:%s StorageAccountType:%s Size:%v", options.DiskName, options.StorageAccountType, options.SizeGB)
-
-	var createZones *[]string
-	if len(options.AvailabilityZone) > 0 {
-		zoneList := []string{d.cloud.GetZoneID(options.AvailabilityZone)}
-		createZones = &zoneList
-		klog.V(4).Infof("azureDisk - AvailabilityZone: %v, zone list: %v", options.AvailabilityZone, zoneList)
-	}
-
-	// insert original tags to newTags
-	newTags := make(map[string]*string)
-	azureDDTag := "kubernetes-azure-dd"
-	newTags["created-by"] = &azureDDTag
-	if options.Tags != nil {
-		for k, v := range options.Tags {
-			// Azure won't allow / (forward slash) in tags
-			newKey := strings.Replace(k, "/", "-", -1)
-			newValue := strings.Replace(v, "/", "-", -1)
-			newTags[newKey] = &newValue
-		}
-	}
-
-	diskSizeGB := int32(options.SizeGB)
-	diskSku := compute.DiskStorageAccountTypes(options.StorageAccountType)
-	creationData, err := getValidCreationData(d.cloud.SubscriptionID, options.ResourceGroup, options.SourceResourceID, options.SourceType)
-	if err != nil {
-		return "", status.Error(codes.Internal, err.Error())
-	}
-
-	diskProperties := compute.DiskProperties{
-		DiskSizeGB:   &diskSizeGB,
-		CreationData: &creationData,
-	}
-
-	if diskSku == compute.UltraSSDLRS {
-		diskIOPSReadWrite := int64(defaultDiskIOPSReadWrite)
-		if options.DiskIOPSReadWrite != "" {
-			v, err := strconv.Atoi(options.DiskIOPSReadWrite)
-			if err != nil {
-				return "", fmt.Errorf("AzureDisk - failed to parse DiskIOPSReadWrite: %v", err)
-			}
-			diskIOPSReadWrite = int64(v)
-		}
-		diskProperties.DiskIOPSReadWrite = to.Int64Ptr(diskIOPSReadWrite)
-
-		diskMBpsReadWrite := int32(defaultDiskMBpsReadWrite)
-		if options.DiskMBpsReadWrite != "" {
-			v, err := strconv.Atoi(options.DiskMBpsReadWrite)
-			if err != nil {
-				return "", fmt.Errorf("AzureDisk - failed to parse DiskMBpsReadWrite: %v", err)
-			}
-			diskMBpsReadWrite = int32(v)
-		}
-		diskProperties.DiskMBpsReadWrite = to.Int32Ptr(diskMBpsReadWrite)
-	} else {
-		if options.DiskIOPSReadWrite != "" {
-			return "", fmt.Errorf("AzureDisk - DiskIOPSReadWrite parameter is only applicable in UltraSSD_LRS disk type")
-		}
-		if options.DiskMBpsReadWrite != "" {
-			return "", fmt.Errorf("AzureDisk - DiskMBpsReadWrite parameter is only applicable in UltraSSD_LRS disk type")
-		}
-	}
-
-	model := compute.Disk{
-		Location: &d.cloud.Location,
-		Tags:     newTags,
-		Zones:    createZones,
-		Sku: &compute.DiskSku{
-			Name: diskSku,
-		},
-		DiskProperties: &diskProperties,
-	}
-
-	if options.ResourceGroup == "" {
-		options.ResourceGroup = d.cloud.ResourceGroup
-	}
-
-	if rerr := d.cloud.DisksClient.CreateOrUpdate(ctx, options.ResourceGroup, options.DiskName, model); rerr != nil {
-		if strings.Contains(rerr.Error().Error(), "NotFound") {
-			return "", status.Errorf(codes.NotFound, "the specify snapshot(%s) is not found under rg(%s), failed with error: %v", options.SourceResourceID, options.ResourceGroup, rerr.Error())
-		}
-		return "", rerr.Error()
-	}
-
-	diskID := ""
-
-	err = kwait.ExponentialBackoff(defaultBackOff, func() (bool, error) {
-		provisionState, id, err := d.cloud.GetDisk(options.ResourceGroup, options.DiskName)
-		diskID = id
-		// We are waiting for provisioningState==Succeeded
-		// We don't want to hand-off managed disks to k8s while they are
-		//still being provisioned, this is to avoid some race conditions
-		if err != nil {
-			return false, err
-		}
-		if strings.ToLower(provisionState) == "succeeded" {
-			return true, nil
-		}
-		return false, nil
-	})
-
-	if err != nil {
-		klog.V(2).Infof("azureDisk - created new MD Name:%s StorageAccountType:%s Size:%v but was unable to confirm provisioningState in poll process", options.DiskName, options.StorageAccountType, options.SizeGB)
-	} else {
-		klog.V(2).Infof("azureDisk - created new MD Name:%s StorageAccountType:%s Size:%v", options.DiskName, options.StorageAccountType, options.SizeGB)
-	}
-
-	return diskID, nil
 }
 
 // Disk name must begin with a letter or number, end with a letter, number or underscore,
