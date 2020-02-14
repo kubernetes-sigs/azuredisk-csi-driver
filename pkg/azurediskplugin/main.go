c151308266974ffe4e6be0ed9b11fbd797ff8e28
ZeroMagic <anthonyliu@zju.edu.cn>
2020-02-14 11:42:24 +0000

    fix: use KubeRegistry instead of prometheus default registry
    Signed-off-by: ZeroMagic <anthonyliu@zju.edu.cn>

--- pkg/azurediskplugin/main.go
+++ pkg/azurediskplugin/main.go
@@ -27,7 +27,7 @@ import (
 
 	"sigs.k8s.io/azuredisk-csi-driver/pkg/azuredisk"
 
-	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"k8s.io/component-base/metrics/legacyregistry"
 	"k8s.io/klog"
 )
 
@@ -93,7 +93,7 @@ func serve(ctx context.Context, l net.Listener, serveFunc func(net.Listener) err
 
 func serveMetrics(l net.Listener) error {
 	m := http.NewServeMux()
-	m.Handle("/metrics", promhttp.Handler())
+	m.Handle("/metrics", legacyregistry.Handler()) //nolint, because azure cloud provider uses legacyregistry currently
 	return trapClosedConnErr(http.Serve(l, m))
 }
 
