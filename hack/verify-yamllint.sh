b4ad765e998d6050ef9f7554e74d2cf3348c77af
andyzhangx <xiazhang@microsoft.com>
2020-02-15 07:40:05 +0000

    doc: fix yamllint format

--- hack/verify-yamllint.sh
+++ hack/verify-yamllint.sh
@@ -21,7 +21,7 @@ fi
 LOG=/tmp/yamllint.log
 helmPath=charts/latest/azuredisk-csi-driver/templates
 
-for path in "deploy/*.yaml" "deploy/example/*.yaml" "deploy/example/metrics/*.yaml"
+for path in "deploy/*.yaml" "deploy/example/*.yaml" "deploy/example/metrics/*.yaml" "deploy/example/snapshot/*.yaml" "deploy/example/cloning/*.yaml" "deploy/example/rawblock/*.yaml"
 do
     echo "checking yamllint under path: $path ..."
     yamllint -f parsable $path | grep -v "line too long" > $LOG
