ceedca95366ecf567143f260a79616dcda64efa1
andyzhangx <xiazhang@microsoft.com>
2020-02-18 03:55:50 +0000

    fix: get max disk count by static table

--- pkg/azuredisk/nodeserver_test.go
+++ pkg/azuredisk/nodeserver_test.go
@@ -19,8 +19,6 @@ package azuredisk
 import (
 	"testing"
 
-	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2019-07-01/compute"
-	"github.com/Azure/go-autorest/autorest/to"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -66,33 +64,28 @@ func TestGetFStype(t *testing.T) {
 func TestGetMaxDataDiskCount(t *testing.T) {
 	tests := []struct {
 		instanceType string
-		sizeList     *[]compute.VirtualMachineSize
 		expectResult int64
 	}{
 		{
 			instanceType: "standard_d2_v2",
-			sizeList: &[]compute.VirtualMachineSize{
-				{Name: to.StringPtr("Standard_D2_V2"), MaxDataDiskCount: to.Int32Ptr(8)},
-				{Name: to.StringPtr("Standard_D3_V2"), MaxDataDiskCount: to.Int32Ptr(16)},
-			},
 			expectResult: 8,
 		},
 		{
+			instanceType: "Standard_DS14_V2",
+			expectResult: 64,
+		},
+		{
 			instanceType: "NOT_EXISTING",
-			sizeList: &[]compute.VirtualMachineSize{
-				{Name: to.StringPtr("Standard_D2_V2"), MaxDataDiskCount: to.Int32Ptr(8)},
-			},
 			expectResult: defaultAzureVolumeLimit,
 		},
 		{
 			instanceType: "",
-			sizeList:     &[]compute.VirtualMachineSize{},
 			expectResult: defaultAzureVolumeLimit,
 		},
 	}
 
 	for _, test := range tests {
-		result := getMaxDataDiskCount(test.instanceType, test.sizeList)
+		result := getMaxDataDiskCount(test.instanceType)
 		assert.Equal(t, test.expectResult, result)
 	}
 }
