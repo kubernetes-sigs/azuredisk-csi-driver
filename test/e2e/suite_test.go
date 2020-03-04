25249c7bde824a40350367d18ec9ece1825f6ec0
andyzhangx <xiazhang@microsoft.com>
2020-03-04 05:01:35 +0000

    test: add more logs in migration e2e test

--- test/e2e/suite_test.go
+++ test/e2e/suite_test.go
@@ -109,7 +109,11 @@ var _ = ginkgo.BeforeSuite(func() {
 })
 
 var _ = ginkgo.AfterSuite(func() {
-	if os.Getenv(driver.AzureDriverNameVar) == "" && testutil.IsRunningInProw() {
+	// Default storage driver configuration is CSI. Freshly built
+	// CSI driver is installed for that case.
+	isUsingInTreeVolumePlugin := os.Getenv(driver.AzureDriverNameVar) == inTreeStorageClass
+	isTestingMigration := os.Getenv(testMigrationEnvVar) != ""
+	if testutil.IsRunningInProw() && (isTestingMigration || !isUsingInTreeVolumePlugin) {
 		azurediskLog := testCmd{
 			command:  "sh",
 			args:     []string{"test/utils/azuredisk_log.sh"},
