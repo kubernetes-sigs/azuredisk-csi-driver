a63038a9afacf6b6aeb8593e068ea88c64955bde
andyzhangx <xiazhang@microsoft.com>
2020-07-08 11:00:26 +0000

    fix: driver installation issue

--- deploy/install-driver.sh
+++ deploy/install-driver.sh
@@ -21,7 +21,7 @@ if [[ "$#" -gt 0 ]]; then
   ver="$1"
 fi
 
-repo="https://raw.githubusercontent.com/kubernetes-sigs/azuredisk-csi-driver/master/deploy"
+repo="https://raw.githubusercontent.com/kubernetes-sigs/azuredisk-csi-driver/$ver/deploy"
 if [[ "$#" -gt 1 ]]; then
   if [[ "$2" == *"local"* ]]; then
     echo "use local deploy"
