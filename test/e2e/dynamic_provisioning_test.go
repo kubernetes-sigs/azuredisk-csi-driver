a7fdb214defbff68b9bc43f159669a540de26332
andyzhangx <xiazhang@microsoft.com>
2020-10-12 03:49:36 +0000

    test: enable one windows test for CSI driver

--- test/e2e/dynamic_provisioning_test.go
+++ test/e2e/dynamic_provisioning_test.go
@@ -262,9 +262,11 @@ func (t *dynamicProvisioningTestSuite) defineTests(isMultiZone bool) {
 	})
 
 	ginkgo.It("should create a deployment object, write and read to it, delete the pod and write and read to it again [kubernetes.io/azure-disk] [disk.csi.azure.com] [Windows]", func() {
-		// waiting for fix(https://github.com/kubernetes/kubernetes/pull/95456) in CSI driver
-		if !isUsingInTreeVolumePlugin || isTestingMigration {
-			ginkgo.Skip("test case is only available for in-tree driver now")
+		if isWindowsCluster {
+			// waiting for fix(https://github.com/kubernetes/kubernetes/pull/95456) in CSI driver
+			if !isUsingInTreeVolumePlugin || isTestingMigration {
+				ginkgo.Skip("test case is only available for in-tree driver now")
+			}
 		}
 
 		pod := testsuites.PodDetails{
