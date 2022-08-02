5b039d4bb2e7640a616a387c95a9f6d83e3ddd72
umagnus <xinyuyuan@microsoft.com>
2022-08-02 04:19:48 +0000

    add fake_hostutil unit test

---
+++ pkg/azureutils/fake_hostutil_test.go
@@ -0,0 +1,41 @@
+package azureutils
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestNewFakeDriver(t *testing.T) {
+	fakeHostUtil := NewFakeHostUtil()
+	fakeHostUtil.SetPathIsDeviceResult("/home/", true, nil)
+
+	tests := []struct {
+		path         string
+		expectedResp bool
+		expectedErr  error
+	}{
+		{
+			"/home/",
+			true,
+			nil,
+		},
+		{
+			"/hame/",
+			false,
+			fmt.Errorf("path %q does not exist", "/hame/"),
+		},
+		{
+			"/mnt/",
+			false,
+			nil,
+		},
+	}
+
+	for _, test := range tests {
+		resultResp, resultErr := fakeHostUtil.PathIsDevice(test.path)
+		if !reflect.DeepEqual(resultResp, test.expectedResp) || (!reflect.DeepEqual(resultErr, test.expectedErr)) {
+			t.Errorf("path: %v, resultResp: %v, expectedResp: %v, resultErr: %v, expectedErr: %v", test.path, resultResp, test.expectedResp, resultErr, test.expectedErr)
+		}
+	}
+}
