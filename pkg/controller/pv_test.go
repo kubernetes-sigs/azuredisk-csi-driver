a1cbef7df0345f783b75c1dbcba44d30a1ba63d6
hccheng72 <huichan0921@gmail.com>
2022-08-26 11:11:24 -0700

    test: add unit tests to pv.go

--- pkg/controller/pv_test.go
+++ pkg/controller/pv_test.go
@@ -19,16 +19,19 @@ package controller
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/wait"
 	fakev1 "k8s.io/client-go/kubernetes/fake"
 	"k8s.io/klog/v2/klogr"
 	azdiskfakes "sigs.k8s.io/azuredisk-csi-driver/pkg/apis/client/clientset/versioned/fake"
+	consts "sigs.k8s.io/azuredisk-csi-driver/pkg/azureconstants"
 	"sigs.k8s.io/azuredisk-csi-driver/pkg/controller/mockclient"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
@@ -64,7 +67,6 @@ func TestPVControllerReconcile(t *testing.T) {
 					mockCtl,
 					testNamespace,
 					azVolume,
-					&testReplicaAzVolumeAttachment,
 					pv)
 
 				mockClients(controller.cachedClient.(*mockclient.MockClient), controller.azClient, controller.kubeClient)
@@ -81,6 +83,119 @@ func TestPVControllerReconcile(t *testing.T) {
 				require.NoError(t, waitErr)
 			},
 		},
+		{
+			description: "[Success] Should not requeue when PV doesn't exist.",
+			request:     testPersistentVolume0Request,
+			setupFunc: func(t *testing.T, mockCtl *gomock.Controller) *ReconcilePV {
+
+				controller := newTestPVController(
+					mockCtl,
+					testNamespace)
+
+				mockClients(controller.cachedClient.(*mockclient.MockClient), controller.azClient, controller.kubeClient)
+
+				return controller
+			},
+			verifyFunc: func(t *testing.T, controller *ReconcilePV, result reconcile.Result, err error) {
+				require.NoError(t, err)
+				require.False(t, result.Requeue)
+			},
+		},
+		{
+			description: "[Success] Should not requeue when PV is marked for deletion and AzVolume doesn't exist.",
+			request:     testPersistentVolume0Request,
+			setupFunc: func(t *testing.T, mockCtl *gomock.Controller) *ReconcilePV {
+				pv := testPersistentVolume0.DeepCopy()
+				pv.Status.Phase = corev1.VolumeReleased
+				pv.DeletionTimestamp = &metav1.Time{Time: time.Now().Add(time.Minute * -1)}
+
+				controller := newTestPVController(
+					mockCtl,
+					testNamespace,
+					pv)
+
+				mockClients(controller.cachedClient.(*mockclient.MockClient), controller.azClient, controller.kubeClient)
+
+				return controller
+			},
+			verifyFunc: func(t *testing.T, controller *ReconcilePV, result reconcile.Result, err error) {
+				require.NoError(t, err)
+				require.False(t, result.Requeue)
+			},
+		},
+		{
+			description: "[Success] Should delete AzVolume when PV is marked for deletion but AzVolume exists.",
+			request:     testPersistentVolume0Request,
+			setupFunc: func(t *testing.T, mockCtl *gomock.Controller) *ReconcilePV {
+				azVolume := testAzVolume0.DeepCopy()
+
+				pv := testPersistentVolume0.DeepCopy()
+				pv.Status.Phase = corev1.VolumeReleased
+				pv.DeletionTimestamp = &metav1.Time{Time: time.Now().Add(time.Minute * -1)}
+
+				controller := newTestPVController(
+					mockCtl,
+					testNamespace,
+					azVolume,
+					pv)
+
+				mockClients(controller.cachedClient.(*mockclient.MockClient), controller.azClient, controller.kubeClient)
+
+				return controller
+			},
+			verifyFunc: func(t *testing.T, controller *ReconcilePV, result reconcile.Result, err error) {
+				require.NoError(t, err)
+				require.False(t, result.Requeue)
+				_, err = controller.azClient.DiskV1beta2().AzVolumes(controller.objectNamespace).Get(context.Background(), testPersistentVolume0Name, metav1.GetOptions{})
+				require.True(t, errors.IsNotFound(err))
+			},
+		},
+		{
+			description: "[Success] Should create AzVolume when PV exists but AzVolume isn't found (for pre-provisioned volume).",
+			request:     testPersistentVolume0Request,
+			setupFunc: func(t *testing.T, mockCtl *gomock.Controller) *ReconcilePV {
+				pv := testPersistentVolume0.DeepCopy()
+				pv.Status.Phase = corev1.VolumeBound
+
+				controller := newTestPVController(
+					mockCtl,
+					testNamespace,
+					pv)
+
+				mockClients(controller.cachedClient.(*mockclient.MockClient), controller.azClient, controller.kubeClient)
+
+				return controller
+			},
+			verifyFunc: func(t *testing.T, controller *ReconcilePV, result reconcile.Result, err error) {
+				require.NoError(t, err)
+				require.False(t, result.Requeue)
+				azv, err := controller.azClient.DiskV1beta2().AzVolumes(controller.objectNamespace).Get(context.Background(), testPersistentVolume0Name, metav1.GetOptions{})
+				require.NoError(t, err)
+				require.Contains(t, azv.Status.Annotations, consts.PreProvisionedVolumeAnnotation)
+			},
+		},
+		{
+			description: "[Success] Should ignore non-csi volumes",
+			request:     testPersistentVolume0Request,
+			setupFunc: func(t *testing.T, mockCtl *gomock.Controller) *ReconcilePV {
+				pv := testPersistentVolume0.DeepCopy()
+				pv.Status.Phase = corev1.VolumeBound
+				pv.Spec.CSI = nil
+
+				controller := newTestPVController(
+					mockCtl,
+					testNamespace,
+					pv)
+
+				mockClients(controller.cachedClient.(*mockclient.MockClient), controller.azClient, controller.kubeClient)
+
+				return controller
+			},
+			verifyFunc: func(t *testing.T, controller *ReconcilePV, result reconcile.Result, err error) {
+				require.NoError(t, err)
+				require.False(t, result.Requeue)
+			},
+		},
 	}
 
 	for _, test := range tests {
@@ -91,6 +206,60 @@ func TestPVControllerReconcile(t *testing.T) {
 			controller := tt.setupFunc(t, mockCtl)
 			result, err := controller.Reconcile(context.TODO(), tt.request)
 			tt.verifyFunc(t, controller, result, err)
+		})
+	}
+}
+
+func TestPVControllerRecover(t *testing.T) {
+	tests := []struct {
+		description string
+		setupFunc   func(*testing.T, *gomock.Controller) *ReconcilePV
+		verifyFunc  func(*testing.T, *ReconcilePV, error)
+	}{
+		{
+			description: "[Success] Should map azVolumeName, pvName, pvClaimName.",
+			setupFunc: func(t *testing.T, mockCtl *gomock.Controller) *ReconcilePV {
+				pv := testPersistentVolume0.DeepCopy()
+
+				controller := newTestPVController(
+					mockCtl,
+					testNamespace,
+					pv)
+
+				mockClients(controller.cachedClient.(*mockclient.MockClient), controller.azClient, controller.kubeClient)
+
+				controller.deletePV(testPersistentVolume0Name)
+				controller.deleteVolumeAndClaim(testPersistentVolume0Name)
+
+				return controller
+			},
+			verifyFunc: func(t *testing.T, controller *ReconcilePV, err error) {
+				require.NoError(t, err)
+
+				pvClaimName := getQualifiedName(testNamespace, testPersistentVolumeClaim0Name)
+				azvName, ok := controller.pvToVolumeMap.Load(testPersistentVolume0Name)
+				require.True(t, ok)
+				require.Equal(t, testPersistentVolume0Name, azvName)
+
+				pvc, ok := controller.volumeToClaimMap.Load(testPersistentVolume0Name)
+				require.True(t, ok)
+				require.Equal(t, pvClaimName, pvc)
+
+				azvName, ok = controller.claimToVolumeMap.Load(pvClaimName)
+				require.True(t, ok)
+				require.Equal(t, testPersistentVolume0Name, azvName)
+			},
+		},
+	}
+
+	for _, test := range tests {
+		tt := test
+		t.Run(tt.description, func(t *testing.T) {
+			mockCtl := gomock.NewController(t)
+			defer mockCtl.Finish()
+			controller := tt.setupFunc(t, mockCtl)
+			err := controller.Recover(context.TODO())
+			tt.verifyFunc(t, controller, err)
 		})
 	}
 }
