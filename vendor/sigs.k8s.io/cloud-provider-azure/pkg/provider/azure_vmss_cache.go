7c774fdc98b0755a5a514b3f5c18a1d7288b19d2
Mingyang Zheng <mizhe@microsoft.com>
2022-09-14 12:46:18 -0700

    scaleSet.OrchestrationMode ==  || scaleSet.OrchestrationMode == compute.OrchestrationModeUniform

--- vendor/sigs.k8s.io/cloud-provider-azure/pkg/provider/azure_vmss_cache.go
+++ vendor/sigs.k8s.io/cloud-provider-azure/pkg/provider/azure_vmss_cache.go
@@ -98,7 +98,7 @@ func (ss *ScaleSet) newVMSSCache() (*azcache.TimedCache, error) {
 					klog.Warning("failed to get the name of VMSS")
 					continue
 				}
-				if scaleSet.OrchestrationMode != compute.OrchestrationModeFlexible {
+				if scaleSet.OrchestrationMode == "" || scaleSet.OrchestrationMode == compute.OrchestrationModeUniform {
 					localCache.Store(*scaleSet.Name, &vmssEntry{
 						vmss:          &scaleSet,
 						resourceGroup: resourceGroup,
