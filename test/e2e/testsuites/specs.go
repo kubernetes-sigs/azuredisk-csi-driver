c9bf0f129f5db78ca684b78eb0df50c57e37285a
andyzhangx <xiazhang@microsoft.com>
2023-02-04 13:51:24 +0000

    test: add dataAccessAuthMode parameter e2e test
    fix

--- test/e2e/testsuites/specs.go
+++ test/e2e/testsuites/specs.go
@@ -300,9 +300,9 @@ func (volume *VolumeDetails) CreateStorageClass(client clientset.Interface, name
 	return tsc, tsc.Cleanup
 }
 
-func CreateVolumeSnapshotClass(client restclientset.Interface, namespace *v1.Namespace, csiDriver driver.VolumeSnapshotTestDriver) (*TestVolumeSnapshotClass, func()) {
+func CreateVolumeSnapshotClass(client restclientset.Interface, namespace *v1.Namespace, parameters map[string]string, csiDriver driver.VolumeSnapshotTestDriver) (*TestVolumeSnapshotClass, func()) {
 	ginkgo.By("setting up the VolumeSnapshotClass")
-	volumeSnapshotClass := csiDriver.GetVolumeSnapshotClass(namespace.Name)
+	volumeSnapshotClass := csiDriver.GetVolumeSnapshotClass(namespace.Name, parameters)
 	tvsc := NewTestVolumeSnapshotClass(client, namespace, volumeSnapshotClass)
 
 	return tvsc, tvsc.Cleanup
