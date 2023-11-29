d1bccb73f8cc02017c91131962f610431fffc68c
andyzhangx <xiazhang@microsoft.com>
2023-11-29 11:11:01 +0000

    test: fix multi-zone test failure on capz multi-zone cluster

--- test/e2e/dynamic_provisioning_test.go
+++ test/e2e/dynamic_provisioning_test.go
@@ -133,7 +133,7 @@ func (t *dynamicProvisioningTestSuite) defineTests(isMultiZone bool) {
 			},
 		}
 
-		if isMultiZone && !isUsingInTreeVolumePlugin {
+		if isMultiZone && !isUsingInTreeVolumePlugin && !isCapzTest {
 			test.StorageClassParameters = map[string]string{
 				"skuName":           "UltraSSD_LRS",
 				"cachingmode":       "None",
@@ -154,7 +154,7 @@ func (t *dynamicProvisioningTestSuite) defineTests(isMultiZone bool) {
 		pods := []testsuites.PodDetails{
 			{
 				Cmd: convertToPowershellorCmdCommandIfNecessary("echo 'hello world' > /mnt/test-1/data && grep 'hello world' /mnt/test-1/data"),
-				Volumes: []testsuites.VolumeDetails{
+				Volumes: t.normalizeVolumes([]testsuites.VolumeDetails{
 					{
 						ClaimSize: "10Gi",
 						VolumeMount: testsuites.VolumeMountDetails{
@@ -163,7 +163,7 @@ func (t *dynamicProvisioningTestSuite) defineTests(isMultiZone bool) {
 						},
 						VolumeAccessMode: v1.ReadWriteOnce,
 					},
-				},
+				}, isMultiZone),
 				IsWindows:    isWindowsCluster,
 				WinServerVer: winServerVer,
 			},
