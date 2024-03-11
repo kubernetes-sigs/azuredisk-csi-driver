d3278edbf7a9e2506e291d23903fb10b417a96a4
andyzhangx <xiazhang@microsoft.com>
2024-03-11 10:43:08 +0000

    vendor: refine

--- vendor/sigs.k8s.io/cloud-provider-azure/pkg/azclient/cloud.go
+++ vendor/sigs.k8s.io/cloud-provider-azure/pkg/azclient/cloud.go
@@ -123,11 +123,14 @@ func GetAzureCloudConfig(armConfig *ARMClientConfig) (*cloud.Configuration, erro
 	if armConfig == nil {
 		return &cloud.AzurePublic, nil
 	}
-	if armConfig.ResourceManagerEndpoint != "" {
-		return AzureCloudConfigFromURL(armConfig.ResourceManagerEndpoint)
-	}
 
-	return AzureCloudConfigOverrideFromEnv(AzureCloudConfigFromName(armConfig.Cloud))
+	config, err := AzureCloudConfigOverrideFromEnv(AzureCloudConfigFromName(armConfig.Cloud))
+	if err == nil && len(armConfig.ResourceManagerEndpoint) > 0 {
+		svcConfig := config.Services[cloud.ResourceManager]
+		svcConfig.Endpoint = armConfig.ResourceManagerEndpoint
+		config.Services[cloud.ResourceManager] = svcConfig
+	}
+	return config, err
 }
 
 // Environment represents a set of endpoints for each of Azure's Clouds.
