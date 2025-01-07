92aa680f674b5e835cf0a9da61671740f2e354ca
mengze zhu <mengzezhu@microsoft.com>
2025-01-07 05:34:18 +0000

    test: add negative case UT for filewatcher

--- pkg/filewatcher/filewatcher_test.go
+++ pkg/filewatcher/filewatcher_test.go
@@ -18,6 +18,7 @@ package filewatcher
 
 import (
 	"os"
+	"strings"
 	"testing"
 	"time"
 )
@@ -28,23 +29,36 @@ func TestWatchFileForChanges(t *testing.T) {
 		// Mock, do nothing
 	}
 
-	// Create a temporary file to watch
-	tmpfile, err := os.CreateTemp("", "testfile")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.Remove(tmpfile.Name())
+	t.Run("ExistingFile", func(t *testing.T) {
+		// Create a temporary file to watch
+		tmpfile, err := os.CreateTemp("", "testfile")
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer os.Remove(tmpfile.Name())
 
-	// Now call the watcher
-	if err = WatchFileForChanges(tmpfile.Name()); err != nil {
-		t.Errorf("Failed to watch file: %v", err)
-	}
+		// Now call the watcher
+		if err = WatchFileForChanges(tmpfile.Name()); err != nil {
+			t.Errorf("Failed to watch file: %v", err)
+		}
 
-	// Trigger the watcher
-	if err = os.WriteFile(tmpfile.Name(), []byte("new content"), 0644); err != nil {
-		t.Fatal(err)
-	}
+		// Trigger the watcher
+		if err = os.WriteFile(tmpfile.Name(), []byte("new content"), 0644); err != nil {
+			t.Fatal(err)
+		}
 
-	// Let the watcher see the change
-	time.Sleep(1 * time.Second)
+		// Let the watcher see the change
+		time.Sleep(100 * time.Millisecond)
+	})
+
+	t.Run("NonExistentFile", func(t *testing.T) {
+		// Reset the watcher once before the test
+		resetWatchCertificateFileOnce()
+
+		err := WatchFileForChanges("nonexistentfile")
+		if err == nil || (!strings.Contains(err.Error(), "no such file or directory") &&
+			!strings.Contains(err.Error(), "The system cannot find the file specified")) {
+			t.Errorf("expected error to contain 'no such file or directory' or 'The system cannot find the file specified', got %v", err)
+		}
+	})
 }
