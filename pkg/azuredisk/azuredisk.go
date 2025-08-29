9e8c80bf66c95ff647d398a264fda513c5125fcb
Lana Andreasyan <laandrea@microsoft.com>
2025-08-28 17:55:42 -0700

    chore: allow active context to issue force detach on timeout
    chore: add pkg tgz and fix linter
    
    chore: pass min detach time in seconds
    
    chore: update helm pkg
    
    chore: don't update charts in this PR
    
    chore: fix time unit after renaming

--- pkg/azuredisk/azuredisk.go
+++ pkg/azuredisk/azuredisk.go
@@ -91,51 +91,52 @@ type Driver struct {
 	csi.UnimplementedIdentityServer
 	csi.UnimplementedNodeServer
 
-	perfOptimizationEnabled      bool
-	cloudConfigSecretName        string
-	cloudConfigSecretNamespace   string
-	customUserAgent              string
-	userAgentSuffix              string
-	cloud                        *azure.Cloud
-	clientFactory                azclient.ClientFactory
-	diskController               *ManagedDiskController
-	eventRecorder                record.EventRecorder
-	migrationMonitor             *MigrationProgressMonitor
-	mounter                      *mount.SafeFormatAndMount
-	deviceHelper                 optimization.Interface
-	nodeInfo                     *optimization.NodeInfo
-	ioHandler                    azureutils.IOHandler
-	hostUtil                     hostUtil
-	useCSIProxyGAInterface       bool
-	enableDiskOnlineResize       bool
-	allowEmptyCloudConfig        bool
-	enableListVolumes            bool
-	enableListSnapshots          bool
-	supportZone                  bool
-	getNodeInfoFromLabels        bool
-	enableDiskCapacityCheck      bool
-	enableTrafficManager         bool
-	trafficManagerPort           int64
-	vmssCacheTTLInSeconds        int64
-	listVMSSWithInstanceView     bool
-	volStatsCacheExpireInMinutes int64
-	attachDetachInitialDelayInMs int64
-	getDiskTimeoutInSeconds      int64
-	vmType                       string
-	enableWindowsHostProcess     bool
-	useWinCIMAPI                 bool
-	getNodeIDFromIMDS            bool
-	enableOtelTracing            bool
-	shouldWaitForSnapshotReady   bool
-	checkDiskLUNCollision        bool
-	checkDiskCountForBatching    bool
-	forceDetachBackoff           bool
-	waitForDetach                bool
-	endpoint                     string
-	disableAVSetNodes            bool
-	removeNotReadyTaint          bool
-	neverStopTaintRemoval        bool
-	kubeClient                   clientset.Interface
+	perfOptimizationEnabled            bool
+	cloudConfigSecretName              string
+	cloudConfigSecretNamespace         string
+	customUserAgent                    string
+	userAgentSuffix                    string
+	cloud                              *azure.Cloud
+	clientFactory                      azclient.ClientFactory
+	diskController                     *ManagedDiskController
+	eventRecorder                      record.EventRecorder
+	migrationMonitor                   *MigrationProgressMonitor
+	mounter                            *mount.SafeFormatAndMount
+	deviceHelper                       optimization.Interface
+	nodeInfo                           *optimization.NodeInfo
+	ioHandler                          azureutils.IOHandler
+	hostUtil                           hostUtil
+	useCSIProxyGAInterface             bool
+	enableDiskOnlineResize             bool
+	allowEmptyCloudConfig              bool
+	enableListVolumes                  bool
+	enableListSnapshots                bool
+	supportZone                        bool
+	getNodeInfoFromLabels              bool
+	enableDiskCapacityCheck            bool
+	enableTrafficManager               bool
+	trafficManagerPort                 int64
+	vmssCacheTTLInSeconds              int64
+	listVMSSWithInstanceView           bool
+	volStatsCacheExpireInMinutes       int64
+	attachDetachInitialDelayInMs       int64
+	DetachOperationMinTimeoutInSeconds int64
+	getDiskTimeoutInSeconds            int64
+	vmType                             string
+	enableWindowsHostProcess           bool
+	useWinCIMAPI                       bool
+	getNodeIDFromIMDS                  bool
+	enableOtelTracing                  bool
+	shouldWaitForSnapshotReady         bool
+	checkDiskLUNCollision              bool
+	checkDiskCountForBatching          bool
+	forceDetachBackoff                 bool
+	waitForDetach                      bool
+	endpoint                           string
+	disableAVSetNodes                  bool
+	removeNotReadyTaint                bool
+	neverStopTaintRemoval              bool
+	kubeClient                         clientset.Interface
 	// a timed cache storing volume stats <volumeID, volumeStats>
 	volStatsCache           azcache.Resource
 	maxConcurrentFormat     int64
@@ -172,6 +173,7 @@ func NewDriver(options *DriverOptions) *Driver {
 	driver.getNodeInfoFromLabels = options.GetNodeInfoFromLabels
 	driver.enableDiskCapacityCheck = options.EnableDiskCapacityCheck
 	driver.attachDetachInitialDelayInMs = options.AttachDetachInitialDelayInMs
+	driver.DetachOperationMinTimeoutInSeconds = options.DetachOperationMinTimeoutInSeconds
 	driver.enableTrafficManager = options.EnableTrafficManager
 	driver.trafficManagerPort = options.TrafficManagerPort
 	driver.vmssCacheTTLInSeconds = options.VMSSCacheTTLInSeconds
@@ -278,6 +280,12 @@ func NewDriver(options *DriverOptions) *Driver {
 		driver.diskController.ForceDetachBackoff = driver.forceDetachBackoff
 		driver.diskController.WaitForDetach = driver.waitForDetach
 		driver.diskController.CheckDiskCountForBatching = driver.checkDiskCountForBatching
+		driver.diskController.DetachOperationMinTimeoutInSeconds = int(driver.DetachOperationMinTimeoutInSeconds)
+		klog.V(2).Infof("set DetachOperationMinTimeoutInSeconds as %d", driver.diskController.DetachOperationMinTimeoutInSeconds)
+		if driver.diskController.DetachOperationMinTimeoutInSeconds <= 0 {
+			klog.V(2).Infof("reset DetachOperationMinTimeoutInSeconds as %d", defaultDetachOperationMinTimeoutInSeconds)
+			driver.diskController.DetachOperationMinTimeoutInSeconds = defaultDetachOperationMinTimeoutInSeconds
+		}
 
 		if kubeClient != nil && driver.NodeID == "" && driver.enableMigrationMonitor {
 			eventBroadcaster := record.NewBroadcaster()
