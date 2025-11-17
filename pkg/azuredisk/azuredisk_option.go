16ae47d2cb43f6834a0c812ddd97da253263cdb7
andyzhangx <xiazhang@microsoft.com>
2025-11-17 13:42:40 +0000

    chore: provide flags for converting ReadWrite cachingMode for in-tree PV
    fix golint

--- pkg/azuredisk/azuredisk_option.go
+++ pkg/azuredisk/azuredisk_option.go
@@ -73,6 +73,7 @@ type DriverOptions struct {
 	ConcurrentFormatTimeout            int64
 	GoMaxProcs                         int64
 	EnableMigrationMonitor             bool
+	ConvertRWCachingModeForIntreePV    bool
 }
 
 func (o *DriverOptions) AddFlags() *flag.FlagSet {
@@ -127,5 +128,6 @@ func (o *DriverOptions) AddFlags() *flag.FlagSet {
 	fs.Int64Var(&o.ConcurrentFormatTimeout, "concurrent-format-timeout", 300, "maximum time in seconds duration of a format operation before its concurrency token is released")
 	fs.Int64Var(&o.GoMaxProcs, "max-procs", 2, "maximum number of CPUs that can be executing simultaneously in golang runtime")
 	fs.BoolVar(&o.EnableMigrationMonitor, "enable-migration-monitor", true, "enable migration monitor for Azure Disk CSI Driver")
+	fs.BoolVar(&o.ConvertRWCachingModeForIntreePV, "convert-rw-caching-mode-for-intree-pv", false, "convert ReadWrite cachingMode to ReadOnly for intree PVs to avoid issues")
 	return fs
 }
