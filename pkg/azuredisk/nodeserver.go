15a5480dee7fbd0acb5fafe8a53d400e32f9f72c
andyzhangx <xiazhang@microsoft.com>
2026-05-03 12:33:30 +0000

    fix: sort imports alphabetically for gofmt

--- pkg/azuredisk/nodeserver.go
+++ pkg/azuredisk/nodeserver.go
@@ -35,8 +35,8 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/wait"
 	cloudprovider "k8s.io/cloud-provider"
